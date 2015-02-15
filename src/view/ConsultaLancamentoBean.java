7d85dbe9f64648d12571b0e93c40a1d26510b229
Manoel Albino Neto <manoelanagr@gmail.com>
2015-02-15 17:15:36 -0200

    imprimirPDF

--- src/view/ConsultaLancamentoBean.java
+++ src/view/ConsultaLancamentoBean.java
@@ -1,14 +1,26 @@
 package view;
 
+import java.io.File;
 import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import javax.annotation.PostConstruct;
 import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 
 import model.Lancamento;
+import net.sf.jasperreports.engine.JRException;
+import net.sf.jasperreports.engine.JRExporterParameter;
+import net.sf.jasperreports.engine.JasperFillManager;
+import net.sf.jasperreports.engine.JasperPrint;
+import net.sf.jasperreports.engine.JasperReport;
+import net.sf.jasperreports.engine.data.JRBeanCollectionDataSource;
+import net.sf.jasperreports.engine.export.JRPdfExporter;
+import net.sf.jasperreports.engine.util.JRLoader;
 
 import org.hibernate.Session;
 import org.hibernate.Transaction;
@@ -49,7 +61,7 @@ public class ConsultaLancamentoBean implements Serializable {
             
             this.inicializar();
             
-            FacesUtil.adicionarMensagem(FacesMessage.SEVERITY_INFO, "LanÃ§amento excluÃ­do com sucesso!");
+            FacesUtil.adicionarMensagem(FacesMessage.SEVERITY_INFO, "Lançamento excluido com sucesso!");
         }
     }
     
@@ -63,6 +75,43 @@ public class ConsultaLancamentoBean implements Serializable {
 
     public void setLancamentoSelecionado(Lancamento lancamentoSelecionado) {
         this.lancamentoSelecionado = lancamentoSelecionado;
+    }
+    
+    public void imprimirPDF(){
+    	JRBeanCollectionDataSource jrds = new JRBeanCollectionDataSource(this.lancamentos);
+    	File arquivoIReport = new File("/Projeto/JSFBasico/src/relatorio/LancamentoRel.jasper");
+    	JasperReport jasperReport = null;
+        try {
+        	jasperReport = (JasperReport) JRLoader.loadObject(arquivoIReport);
+		} catch (JRException e) {
+			FacesUtil.adicionarMensagem(FacesMessage.SEVERITY_ERROR, "Deu Merda");
+		} 
+        Map parametros = new HashMap();
+        parametros.put("tituloRelatorio", "Lançamentos");
+        parametros.put("logoPadraoRelatorio", "/Projeto/JSFBasico/WebContent/imagens/logo.png");
+        JasperPrint printer = null;
+		try {
+			printer = JasperFillManager.fillReport(jasperReport, parametros, jrds);
+		} catch (JRException e1) {
+			FacesUtil.adicionarMensagem(FacesMessage.SEVERITY_ERROR, "Erro");
+		}       
+        File pdfFile = new File("/Projeto/JSFBasico/src/relatorio/"+ new Date().getTime() + "pdf");
+            if (pdfFile.exists()) {
+                try {
+                    pdfFile.delete();
+                } catch (Exception e) {
+                	FacesUtil.adicionarMensagem(FacesMessage.SEVERITY_ERROR, "Erro ao exportar arquivo em PDF");
+                }
+            }
+		JRPdfExporter jrpdfexporter = new JRPdfExporter();
+		jrpdfexporter.setParameter(JRExporterParameter.JASPER_PRINT, printer);
+		jrpdfexporter.setParameter(JRExporterParameter.OUTPUT_FILE, pdfFile);
+		try{
+            jrpdfexporter.exportReport();
+        }catch(Exception erro){
+        	FacesUtil.adicionarMensagem(FacesMessage.SEVERITY_ERROR, erro.getMessage());
+        	
+        }        
     }
     
 }
